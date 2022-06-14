d03f7195f2639bdce7837e1c008aef1681b7553b
eververseme <55100330+eververseme@users.noreply.github.com>
2022-06-14 12:07:59 -0500

    Create geo.js

---
+++ geo.js
@@ -0,0 +1,28 @@
+(async function () {
+  /**
+   * Get data based on IP
+   * @param {string} [ip] - IP, default is current
+   * @returns {Object} - fields of API response (https://ipapi.co/api/#complete-location)
+   */
+  const getLocale = async function getLocale(ip) {
+    let url = 'https://ipapi.co/';
+    if (ip) {
+      url += ip + '/';
+    }
+    url += 'json/';
+    return fetch(url).then(function (response) {
+      return response.json();
+    });
+  };
+
+  //Get locale data
+  const locale = await getLocale().catch(function (err) {
+    console.log(err);
+  });
+
+  //Display info
+  document.getElementsByTagName('body')[0].innerHTML += [
+  locale.city
+  ].join('');
+})();
+
